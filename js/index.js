e97613546cec3f301d85f3dfbb182cab30c5899d
Alex <aleexsantos1509@gmail.com>
2024-01-22 13:18:22 +0100

    Updated styles. Animated header when scrolling down

--- js/index.js
+++ js/index.js
@@ -11,7 +11,31 @@ const navMenu = document.querySelector(".nav-menu");
 let menuOpen = false;
 
 burgerIcon.addEventListener("click", () => {
-        navMenu.classList.toggle("active");
-        burgerIcon.classList.toggle("open");
-        menuOpen = !menuOpen;
+    navMenu.classList.toggle("active");
+    burgerIcon.classList.toggle("open");
+    menuOpen = !menuOpen;
+});
+
+// HIDE HEADER ON SCROLL ANIMATION
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+const delta = 100;
+
+window.addEventListener("scroll", function () {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    // No hacer nada si el scroll es menor que delta
+    if (Math.abs(lastScrollTop - scrollTop) <= delta) {
+        return;
+    }
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll hacia abajo
+        header.classList.add("scroll-down");
+    } else {
+        // Scroll hacia arriba
+        header.classList.remove("scroll-down");
+    }
+
+    lastScrollTop = scrollTop;
 });
